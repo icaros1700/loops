0353cbdf5bf7815951104ec08be20454b9ad38fd
William Ruiz <william.a23@gmail.com>
2019-12-03 11:42:31 -0500

    Se adiciona la prueba del bucle for

---
+++ for.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+  
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+for ($i=0;$i<=10;$i++) {
+
+    echo "9 x $i = " . 9*$i . "<br>";
+
+}
+
+echo "Te hemos presentado la tabla del 9";
+
+?>
+
+
+    
+</body>
+</html>
