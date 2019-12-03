6852236086fe95e317e826b2207c4bb5cee242d2
William Ruiz <william.a23@gmail.com>
2019-12-03 10:58:25 -0500

    prueba de do while

---
+++ Do while.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$var1=7;
+
+do {
+
+    echo "Cumple la condicion <br>";
+
+    $var1++;
+} while($var1<6);
+
+echo "No cumple la condicion";
+
+?>
+    
+</body>
+</html>
