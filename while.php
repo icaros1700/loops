8f025815cafecf9b3b349453a4527e206ad25f03
William Ruiz <william.a23@gmail.com>
2019-12-03 10:46:52 -0500

    Prueba de loops

---
+++ while.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$var1=1;
+
+while ($var1<6) {
+
+    echo "Cumple la Condicion <br>";
+
+    $var1++;
+}
+
+echo "Dejo de Cumplir la Condicion";
+
+?>
+    
+</body>
+</html>
